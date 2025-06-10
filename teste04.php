035de79ef185c692174bb26bcc70dc13ee7e62a1
Kauane1706 <kauanekakadossantos8542gmail.com>
2025-06-10 08:53:17 -0300

    adiciona exemplo de estrutura de controle while em teste04.php

---
+++ teste04.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estrutura de controle</title>
+</head>
+<body>
+    <h1>while</h1>
+    Números pares até dez:
+    <?php
+    $i = 2;
+    whilw ($i <=´10){
+        echo $i.",";
+        $i = $i + 2;
+    }
+    ?>
+</body>
+</html>
