d7428aeb42193cb4673bc58840b9be7e7f044368
Kauane1706 <kauanekakadossantos8542gmail.com>
2025-06-10 09:24:25 -0300

    adiciona exemplo de uso de PHP com controle de horário em ex001.php

---
+++ ex001.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo com PHP</title>
+</head>
+<body>
+    <?php
+    date_default_timezone_set('America/Sao_Paulo');
+    $hoje = date('d/m/Y');
+    $agora = date('H:i:s');
+    $hora = date('H');
+    if ($hora < 6 or $hora > 18){
+        echo "<img src= lua.png>";
+    } else {
+        echo "<img src= sol.png>";
+    }
+    echo "Bom dia!". $hoje. " e agora são". $agora. " horas.";
+    ?>
+</body>
+</html>
