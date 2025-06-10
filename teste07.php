464a4634fbdb8aefc366584c48b38f9c5c6d07d6
Kauane1706 <kauanekakadossantos8542gmail.com>
2025-06-10 09:04:31 -0300

    adiciona exemplo de função PHP em teste07.php

---
+++ teste07.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções</title>
+</head>
+<body>
+    <h1>Funções</h1>
+    <?php
+    function soma($i, $j){
+        $k = $i + $j;
+        return $k;
+    }
+    $res = soma(4,5);
+    echo "O resultado é ".$res;
+    ?>
+</body>
+</html>
